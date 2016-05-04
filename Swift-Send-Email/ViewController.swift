ed6691e1a28a379fac5f9ed9491432b7ba8ed4ee
Ronny Soltveit <ronny@ronnys-macbook-air.local>
2016-05-04 23:43:41 +0200

    Initial Commit

---
+++ Swift-Send-Email/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Send-Email
+//
+//  Created by Ronny Soltveit on 04/05/16.
+//  Copyright © 2016 Ronny Soltveit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
