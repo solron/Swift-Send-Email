cdacdec45404f2d7b299369f50dd91c1c643c0a6
Ronny <ronny@soltveit.org>
2016-05-05 01:12:54 +0200

    Working example

--- Swift-Send-Email/ViewController.swift
+++ Swift-Send-Email/ViewController.swift
@@ -7,8 +7,19 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
+    @IBAction func btnSendEmail(sender: AnyObject) {
+        let email = MFMailComposeViewController()
+        email.mailComposeDelegate = self
+        email.setSubject("My subject")
+        email.setMessageBody("Some example text", isHTML: false) // or true, if you prefer
+        email.setToRecipients(["ronny@soltveit.org"])
+        if MFMailComposeViewController.canSendMail() {
+            presentViewController(email, animated: true, completion: nil)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +31,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
